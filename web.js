645e41ae5bcc7346479f69bd70864c940b7ad447
Ramji <ramji.edu@gmail.com>
2013-07-11 17:49:04 +0000

    Updated index.html

--- web.js
+++ web.js
@@ -2,10 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buf = new Buffer(fs.writeFileSync('index.html', "Hello World from index.html"), "utf-8");
-
 app.get('/', function(request, response) {
-  response.send("HTML WORLD"));
+  response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
