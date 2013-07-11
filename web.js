04fd38534d61d89ee079d605e458ea123cec605b
Ramji <ramji.edu@gmail.com>
2013-07-11 06:19:14 +0000

    Updated Hello world

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buf = new Buffer(256);
+var buflength = buf.write("Hello World from index.html", "utf-8");
+var bufText = buf.toString("utf-8", 0, buflength);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fs.writeFileSync('index.html', bufText));
 });
 
 var port = process.env.PORT || 5000;
