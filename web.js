f010431abe3a557c7424cfc8330a4acd4fabbb04
Ramji <ramji.edu@gmail.com>
2013-07-11 06:37:25 +0000

    var refered

--- web.js
+++ web.js
@@ -6,8 +6,10 @@ var buf = new Buffer(256);
 var buflength = buf.write("Hello World from index.html", "utf-8");
 var bufText = buf.toString("utf-8", 0, buflength);
 
+fs.writeFileSync('index.html', bufText);
+
 app.get('/', function(request, response) {
-  response.send(fs.writeFileSync('index.html', "Hello World from index.html"));
+  response.send(index.html);
 });
 
 var port = process.env.PORT || 5000;
