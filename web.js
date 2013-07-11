f3183ebc727aa86605085ac68b8354ac13881538
Ramji <ramji.edu@gmail.com>
2013-07-11 07:00:39 +0000

    var refered_3

--- web.js
+++ web.js
@@ -2,19 +2,17 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buf = new Buffer(256);
-var buflength = buf.write("Hello World from index.html", "utf-8");
-var bufText = buf.toString("utf-8", 0, buflength);
+var buf = new Buffer("Hello World from index.html", "utf-8");
+//var buflength = buf.write("Hello World from index.html", "utf-8");
+//var bufText = buf.toString("utf-8", 0, buflength);
 
-fs.writeFileSync('index.html', bufText);
+fs.writeFileSync('index.html', buf.toString());
 
 app.get('/', function(request, response) {
-  response.send(index.html);
+  response.send('index.html');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-
