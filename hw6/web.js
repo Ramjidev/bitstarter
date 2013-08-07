ceae86082103dcf7b05473a6f85bd3728a0a0489
Ubuntu <ubuntu@ip-172-31-46-185.us-west-2.compute.internal>
2013-08-07 03:58:23 +0000

    Empty directory

---
+++ hw6/web.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+
+  var fs = require('fs');
+  var text = fs.readFileSync('index.html');
+  var buf = new Buffer (text, "utf-8");
+
+  response.send(buf.toString());
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
