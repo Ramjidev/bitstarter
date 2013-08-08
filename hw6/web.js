ba0d6111eea9229478051d84ae07142e135a35e3
Ubuntu <ubuntu@ip-172-31-46-185.us-west-2.compute.internal>
2013-08-08 02:54:55 +0000

    updated web.js and index.html to fix bugs

--- hw6/web.js
+++ hw6/web.js
@@ -1,17 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+var htmlfile = 'index.html';
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-
-  var fs = require('fs');
-  var text = fs.readFileSync('index.html');
-  var buf = new Buffer (text, "utf-8");
-
-  response.send(buf.toString());
+app.get('/', function(request, response){
+	var html = fs.readFileSync(htmlfile).toString();
+	response.send(html);
 });
 
 var port = process.env.PORT || 8080;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function(){
+	console.log("Listening on " + port);
 });
